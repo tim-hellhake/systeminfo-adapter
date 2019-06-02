da7485c19165bf8d90f1bc512f937e4df483d87d
Tim Hellhake <tim-hellhake@users.noreply.github.com>
2019-06-02 23:45:52 +0200

    Release initial version 0.1.0

---
+++ README.md
@@ -0,0 +1,9 @@
+# Systeminfo Adapter
+
+[![Build Status](https://travis-ci.org/tim-hellhake/systeminfo-adapter.svg?branch=master)](https://travis-ci.org/tim-hellhake/systeminfo-adapter)
+[![dependencies](https://david-dm.org/tim-hellhake/systeminfo-adapter.svg)](https://david-dm.org/tim-hellhake/systeminfo-adapter)
+[![devDependencies](https://david-dm.org/tim-hellhake/systeminfo-adapter/dev-status.svg)](https://david-dm.org/tim-hellhake/systeminfo-adapter?type=dev)
+[![optionalDependencies](https://david-dm.org/tim-hellhake/systeminfo-adapter/optional-status.svg)](https://david-dm.org/tim-hellhake/systeminfo-adapter?type=optional)
+[![license](https://img.shields.io/badge/license-MPL--2.0-blue.svg)](LICENSE)
+
+Get stats of the system.
---
+++ systeminfo-adapter.js
@@ -0,0 +1,75 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.*
+ */
+
+'use strict';
+
+const si = require('systeminformation');
+
+const {
+  Adapter,
+  Device,
+  Property
+} = require('gateway-addon');
+
+class Systeminfo extends Device {
+  constructor(adapter, manifest) {
+    super(adapter, Systeminfo.name);
+    this['@context'] = 'https://iot.mozilla.org/schemas/';
+    this.name = manifest.display_name;
+    this.description = manifest.description;
+
+    this.addProperty('mem_available', {
+      type: 'number',
+      unit: 'GB',
+      title: 'Available memory',
+      description: 'Available memory',
+      readOnly: true
+    });
+  }
+
+  addProperty(name, description) {
+    const property = new Property(this, name, description);
+    this.properties.set(name, property);
+  }
+
+  setProperty(name, value) {
+    const property = this.properties.get(name);
+
+    if (property) {
+      property.setCachedValue(value);
+      this.notifyPropertyChanged(property);
+    } else {
+      console.warn(`Property ${name} not found`);
+    }
+  }
+
+  startPolling(interval) {
+    this.poll();
+    this.timer = setInterval(() => {
+      this.poll();
+    }, interval * 1000);
+  }
+
+  async poll() {
+    const {
+      available
+    } = await si.mem();
+
+    this.setProperty('mem_available', available / 1024 / 1024 / 1024);
+  }
+}
+
+class SysteminfoAdapter extends Adapter {
+  constructor(addonManager, manifest) {
+    super(addonManager, SysteminfoAdapter.name, manifest.name);
+    addonManager.addAdapter(this);
+    const device = new Systeminfo(this, manifest);
+    this.handleDeviceAdded(device);
+    device.startPolling(1);
+  }
+}
+
+module.exports = SysteminfoAdapter;
